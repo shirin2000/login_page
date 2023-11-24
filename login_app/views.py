11e9e295cf8f635a92344743a3e7f2744ceeeb9d
shirin <shirinsaleem2000@gmail.com>
2023-11-25 03:12:18 +0530

    first commit

---
+++ login_app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+
+# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    return render(request, 'login.html')
+
+def home_view(request):
+    return render(request, 'home.html')
